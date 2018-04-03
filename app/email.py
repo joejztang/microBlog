f899b38f377be270ad5078e01d4a9b449c6595c9
joejztang <joejztang@gmail.com>
2018-04-03 11:47:34 -0700

    10 finishes

--- app/email.py
+++ app/email.py
@@ -1,12 +1,26 @@
+# The threading and multiprocessing modules can both do this. 
+# Starting a background thread for email being sent is much less 
+# resource intensive than starting a brand new process
+from threading import Thread
 from flask_mail import Message
 from flask import render_template
 from app import app, mail
 
+def send_async_mail(app, msg):
+	with app.app_context():
+		mail.send(msg)
+
+
+# but know that there are two types of contexts, 
+# the application context and the request context. 
+# In most cases, these contexts are automatically managed by the framework, 
+# but when the application starts custom threads, 
+# contexts for those threads may need to be manually created.
 def send_mail(subject, sender, recipients, text_body, html_body):
 	msg = Message(subject, sender=sender, recipients=recipients)
 	msg.body = text_body
 	msg.html = html_body
-	mail.send(msg)
+	Thread(target=send_async_mail, args=(app, msg)).start()
 
 def send_password_reset_email(user):
 	token = user.get_reset_password_token()
