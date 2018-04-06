e59ecefbefe21c91f73a112f8bc4511648955534
joejztang <joejztang@gmail.com>
2018-04-06 16:34:26 -0700

    fix bugs and refactor done, but unittest is not done

--- app/main/routes.py
+++ app/main/routes.py
@@ -42,7 +42,7 @@ def index():
 @login_required
 def explore():
 	page = request.args.get('page', 1, type=int)
-	posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+	posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, current_app.config['POSTS_PER_PAGE'], False)
 	next_url = url_for('main.explore', page=posts.next_num) if posts.has_next else None
 	prev_url = url_for('main.explore', page=posts.prev_num) if posts.has_prev else None
 	return render_template('index.html', title=_('Explore'), posts=posts.items, next_url=next_url, prev_url=prev_url)
